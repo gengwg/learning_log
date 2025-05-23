b76de8c07a31ad7038debdb35be6641270970275
gengwg <genwg@users.noreply.github.com>
2025-05-23 10:38:59 -0700

    create the registration page

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
     # Include the default auth URLs
     path('', include('django.contrib.auth.urls')),
+    # Register page
+    path('register/', views.register, name='register'),
 ]
